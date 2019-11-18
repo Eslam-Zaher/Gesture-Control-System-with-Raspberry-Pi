dd742b648a66be6d467f387bc605779c4bf2ca5d
Eslam Zaher <39067250+Eslam-Zaher@users.noreply.github.com>
2019-11-19 01:41:22 +0200

    Update AngleOMeter.py
    Servo teshta3'al

--- AngleOMeter.py
+++ AngleOMeter.py
@@ -170,18 +170,12 @@ while True:
 	except Exception as exc:
 		flag += 1
 
+	# if the y angles is more than 45 degrees do not do anything
+	# else
+	# we move the servo
+	
 	if kalAngleY <= 45:
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(11, GPIO.OUT)
-        p = GPIO.PWM(11, 50)
-        p.start(0)
-        duty = kalAngleX/18.0 + 2.0
-        GPIO.output(11, True)
-        p.ChangeDutyCycle(duty)
-        sleep(1)
-        GPIO.output(11, False)
-        p.ChangeDutyCycle(0)
-        p.stop()
+		Servo.Drive(kalAngleX=kalAngleX)
 
     GPIO.cleanup()
     time.sleep(0.005)
